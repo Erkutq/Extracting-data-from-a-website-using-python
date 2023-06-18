466224dd8f31c86e1f87015e50bbde73be7d977e
Erkut Elik <erkutelik@gmail.com>
2023-06-18 13:51:54 +0300

    Data pulled from website

---
+++ from_website.py
@@ -0,0 +1,7 @@
+class Pro():
+    def __init__(self,isim,soyisim,numara) -> None:
+        self.isim=isim
+        self.soyisim=soyisim
+        self.numara=numara
+        print("Kullanıcı bilgileri alındı")
+    
