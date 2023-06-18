0f3b288497731d08bcbef352a2b73bb4c6c2638b
Erkut Elik <erkutelik@gmail.com>
2023-06-18 14:15:34 +0300

    Hava durumu bilgilerini çekme

--- from_website.py
+++ from_website.py
@@ -1,7 +1,20 @@
-class Pro():
-    def __init__(self,isim,soyisim,numara) -> None:
-        self.isim=isim
-        self.soyisim=soyisim
-        self.numara=numara
-        print("Kullanıcı bilgileri alındı")
+from bs4 import BeautifulSoup
+import requests
+
+class Veri:
+    def __init__(self,url) -> None:
+        self.url=url
+        self.html=requests.get(self.url).content
+        self.soup=BeautifulSoup(self.html,"html.parser")
+        Veri.veriGetir(self)
+
+    def veriGetir(self):
+        getir=self.soup.select("div.day-detail >ul>li>div")
+        isim=self.soup.select("div.day-detail >ul>li>div>span:nth-child(2)")
+        for a,b in zip(getir,isim):
+            tarih=(a.span.text)
+            derece=(b.text)
+            print(f"İsimler > {tarih} Derece > {derece}")
+
     
+Veri("https://www.haberturk.com/havadurumu/Turkiye-TR/Istanbul/LTSI")
